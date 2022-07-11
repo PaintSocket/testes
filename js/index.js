f172868d8400c2e9fa69396ef5a38d2311b4722f
facobackup <gustavomicaelbarbosa@gmail.com>
2022-07-11 16:29:26 -0300

    Inicializando app

---
+++ js/index.js
@@ -0,0 +1,14 @@
+window.onload = function (){
+    const formID = document.getElementById("form-sign-in")
+    const formTag = document.getElementsByTagName("form")
+    const formClass = document.getElementsByClassName("form")
+
+    const classSelector = document.querySelector(".input")
+    const idSelector = document.querySelector("#id")
+    const tagSelector = document.querySelector("input")
+
+    const tagMultiSelector = document.querySelectorAll("input")
+    const classMultiSelector = document.querySelectorAll(".input")
+
+
+}
